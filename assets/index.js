e42025e4469a99445758d4077480cb59e0acd418
Grenish rai <ariestheracer@gmail.com>
2023-09-24 04:14:43 +0530

    initial update

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+import skinscanLogo from "./skinscanLogo.png";
+import gallery from "./gallery.png";
+import camera from "./cameraskin.png";
+import settings from "./settings.png";
+export { skinscanLogo, gallery, camera, settings };
